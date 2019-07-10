6d7378e9922588b9810ca75e2700dd002e159b7a
eric-Is-Me <echai009@ucr.edu>
2019-07-10 12:03:04 -0700

    Added functionality to upload button. Allows users to select a file to upload

--- scripts.js
+++ scripts.js
@@ -3,4 +3,8 @@ let mainNav = document.getElementById('menu');
 function changeMenu(x) {
   x.classList.toggle("change");
   mainNav.classList.toggle('active');
-}
+};
+
+function uploadFunction() {
+	document.getElementById('my_file').click();
+};
