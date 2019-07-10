3243eb7259f82e23b2c12e6bc2c052f56e803db9
eric-Is-Me <echai009@ucr.edu>
2019-07-10 15:12:36 -0700

    Successfully connected Firebase for upload function. Added image grid filled with placeholder, hardcoded, sample images.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
 let mainNav = document.getElementById('menu');
+let selectedFile = null;
 
 function changeMenu(x) {
-  x.classList.toggle("change");
+  x.classList.toggle('change');
   mainNav.classList.toggle('active');
 };
 
 function uploadFunction() {
-	document.getElementById('my_file').click();
+	document.getElementById('upFile').click();
 };
+
+function handleFileUploadChange(e) {
+	selectedFile = e.target.files[0];
+	const uploadTask = storageRef.child('images/${selectedFile.name}').put(selectedFile);
+
+	uploadTask.on('state_changed', (error) => {
+	// Handle unsuccessful uploads
+	console.log(error);
+	}, () => {
+	 // Do something once upload is complete
+	 console.log('success');
+	});
+};
+
+document.querySelector('#upFile').addEventListener('change', handleFileUploadChange);
